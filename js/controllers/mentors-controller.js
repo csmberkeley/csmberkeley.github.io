9bddb65c49ccc2cdb779fa4ed2410079bda3fdd1
colbyguan <colbyguan@gmail.com>
2016-12-03 17:10:48 -0800

    updates for Spring 2017 JM

--- js/controllers/mentors-controller.js
+++ js/controllers/mentors-controller.js
@@ -1,12 +1,14 @@
 (function() {
   angular.module('CSM')
   .controller('MentorsController', function($scope) {
-    $scope.applicationsOpen = false;
+    $scope.applicationsOpen = true;
     $scope.events = [
-      {
-        date: "1/1",
-        title: "Sample event"
-      }
+      { 'date': '1/15 11:59 PM', 'title': 'Apps due' },
+      { 'date': '1/16 11:59 PM', 'title': 'Interview notifications' },
+      { 'date': '1/17 - 1/20', 'title': 'Interviews' },
+      { 'date': '1/22', 'title': 'Interview results' },
+      { 'date': '1/29', 'title': 'Orientation' },
+      { 'date': '2/5', 'title': 'Mentoring beings' }
     ];
   });
 })();
