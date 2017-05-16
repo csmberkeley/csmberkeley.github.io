a47fd1108d0010e2d89381eea3b86c4c4d0b314c
James Jiang <jmendre@berkeley.edu>
2017-05-15 22:50:56 -0700

    fall2017 applications now open with updated calendar

--- js/controllers/mentors-controller.js
+++ js/controllers/mentors-controller.js
@@ -1,14 +1,20 @@
 (function() {
   angular.module('CSM')
   .controller('MentorsController', function($scope) {
-    $scope.applicationsOpen = false;
+    $scope.applicationsOpen = true;
     $scope.events = [
-      { 'date': '1/15 11:59 PM', 'title': 'Apps due' },
-      { 'date': '1/16 11:59 PM', 'title': 'Interview notifications' },
-      { 'date': '1/17 - 1/20', 'title': 'Interviews' },
-      { 'date': '1/22', 'title': 'Interview results' },
-      { 'date': '1/29', 'title': 'Orientation' },
-      { 'date': '2/5', 'title': 'Mentoring begins' }
+      { 'date': '8/20 11:59 PM', 'title': 'Apps due' },
+      { 'date': '8/21 11:59 PM', 'title': 'Interview notifications' },
+      { 'date': '8/22 - 8/24', 'title': 'Interviews (CS70)' },
+      { 'date': '8/22 - 8/25', 'title': 'Interviews (CS61A CS61B)'},
+      { 'date': '8/25', 'title': 'Interview results (CS70)' },
+      { 'date': '8/27', 'title': 'Interview results (CS61A CS61B)' },
+      { 'date': '8/27', 'title': 'Mandatory Meeting (CS70)' },
+      { 'date': '8/28', 'title': 'Meeting with Senior Mentors Begans (CS70)' },
+      { 'date': '9/4', 'title': 'Orientation' },
+      { 'date': '9/5', 'title': 'Meetings with Senior Mentors begin' },
+      { 'date': '9/5', 'title': 'Mentoring begins (CS70)' },
+      { 'date': '9/11', 'title': 'Mentoring begins (CS61A CS61B)'}
     ];
   });
 })();
